c88fb2de2b0fb8ba6aeca89a9d7e55404c021bbb
joshpalao20 <joshpalao21@gmail.com>
2025-08-28 10:28:50 +0800

    color for buttons

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#1062c0ff',
     color: 'white',
     borderRadius: 8,
   }
